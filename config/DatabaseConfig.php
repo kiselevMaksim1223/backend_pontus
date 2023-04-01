7aa8222c1ebacf2c6a956c7f9017a73b72ccd1d6
Maksim_Kiselev <laurkurt@gmail.com>
2023-04-01 15:01:05 +0300

    add env var

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -1,6 +1,6 @@
 <?php
 //create var for database
 define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_NAME', 'hotelPontus');

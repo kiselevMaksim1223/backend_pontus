ceb42a56370348bbaa0d1f21c0231da8cb1c881a
Maksim_Kiselev <laurkurt@gmail.com>
2023-04-08 18:10:37 +0300

    database config

--- config/DatabaseConfig.php
+++ config/DatabaseConfig.php
@@ -2,5 +2,5 @@
 //create var for database
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_USERNAME', $_ENV['DB_USERNAME']);
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
 define('DB_NAME', 'hotelPontus');
